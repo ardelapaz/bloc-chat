121d99cf893b9026d12d6cec9d7419ed02c0b87f
Andrew Delapaz <ardelapaz@yahoo.com>
2018-02-12 19:01:07 -0500

    Completed basic list chat rooms checkpoint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+import { Route, Link } from 'react-router-dom';
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCGq3M4KVfzbwmnZix0Uz-I3LdEIyZrUE4",
+    authDomain: "bloc-chat-13193.firebaseapp.com",
+    databaseURL: "https://bloc-chat-13193.firebaseio.com",
+    projectId: "bloc-chat-13193",
+    storageBucket: "bloc-chat-13193.appspot.com",
+    messagingSenderId: "288716000715"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          {/* <Route exact path="/" component={RoomList} /> */} <RoomList firebase= {firebase}></RoomList>
+          </main>
       </div>
     );
   }
