69ea7b419f52a6bc004e9ce06c0573815c81e91f
Andrew Delapaz <ardelapaz@yahoo.com>
2018-02-19 21:19:52 -0500

    'test'

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -3,14 +3,53 @@ import * as firebase from 'firebase';
 
 
 
+
 class MessageList extends Component {
     constructor(props){
         super(props);
-    }
+        this.state = 
+            {
+             messages: [],
+             newMessage: ''
+            }
+            this.messagesRef = firebase.database().ref('messages');
+            this.createMesssage = this.createMessage.bind(this);
+        }
+
+        componentDidMount() {
+            this.messagesRef.on('child_added', snapshot => {
+                const message = snapshot.val();
+                message.key = snapshot.key;
+                this.setState({ messages: this.state.messages.concat(message)});
+            });
+        } 
+        createMessage() {
+            console.log("lh");
+            this.messagesRef.push({
+                username: "<USERNAME HERE>",
+                content: this.state.newMessage,
+                sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
+                roomID: this.props.activeRoom
+              });
+              this.setState({newMessage: ''});
+
+        }
+
+        onTextChange(e) {
+            const message = e.target.value;
+            this.setState({ newMessage: message})
+        }
+
+
     render() {
         return (
-            <div>{this.props.activeRoom}</div>
+            <div>{this.props.activeRoom}
+            <form id="messageContent" onSubmit={(e) => {e.preventDefault(); this.createMessage()}} >
+            Message: <input type="text" id = "messageContent" value = {this.state.newMessage} onChange = {this.onTextChange.bind(this)} ></input>
+            <input type="submit"/>            
+            </form>
+            </div>
         )
     }
 }
-export default MessageList;
+export default MessageList;              
